a055d329a41af4085ca84b0e4ad29d6e2bc6c891
Rishabh Jain <rishabh.k.jain@outlook.com>
2019-04-19 09:17:23 -0700

    exeception handling

--- exception_handling.py
+++ exception_handling.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 # Look for built in errors in python documentation
+# Syntax Error and Exception Error
+# Raising an Exception
+# assert a condition is met or raise Assertion Error
+
+# assert
+import sys
+assert('darwin' in sys.platform), "This code runs in mac only"
+
+#raise
+try:
+    raise Exception("You have entered an invalid number")
+except Exception as exc:
+    ##Printing Exception Name
+    print('Exception Name: ',exc.__class__.__name__)
+    print('Message: ',exc)
+
+
+# try,except,else finally
 try:
     x = int(input("Enter a number here: "))
 except ValueError as err:
