a66f47a23cc7de5006cb9e11a6ac0754fc906cea
Rishabh Jain <rishabh.k.jain@outlook.com>
2019-09-21 19:02:43 -0700

    add content for decorators

--- 114-generators.py
+++ 114-generators.py
@@ -29,3 +29,35 @@ print(next(itr_my_list))
 print(next(itr_my_list))
 print(next(itr_my_list))
 print(next(itr_my_list))
+
+
+
+def get_square(n):
+	my_list = []
+	for i in n:
+		my_list.append(i * i)
+	return my_list
+
+
+print(get_square([1,2,3,4,5,6,7]))
+print(type(get_square([1,2,3,4,5,6,7])))
+
+
+def yield_square(n):
+	for i in n:
+		yield i*i
+
+x = yield_square([1,2,3,4,5,6,7])
+print(type(x))
+
+
+for i in x:
+	print(i)
+
+
+some_list = (x*x for x in [5,4,3,2,1])
+print(type(some_list))
+
+for i in some_list:
+	print(i)
+
